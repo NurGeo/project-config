e220d9cb1f4eb93a9a35249f48728f47826a2297
yur.lug <ura-roma@mail.ru>
2021-06-21 15:53:47 +0500

    Добавлены common-files

---
+++ src/common-files/jest.all-config.js
@@ -0,0 +1,5 @@
+const allJestConfig = require('./node_modules/@buildozer/project-configs/src/base-configs/backend/jest.all-config');
+
+module.exports = {
+  ...allJestConfig,
+};
---
+++ src/common-files/jest.unit-config.js
@@ -0,0 +1,5 @@
+const unitJestConfig = require('./node_modules/@buildozer/project-configs/src/base-configs/backend/jest.unit-config');
+
+module.exports = {
+  ...unitJestConfig,
+};
