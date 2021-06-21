d65c390644d95b75355bb375c880f015109f0d41
yur.lug <ura-roma@mail.ru>
2021-06-21 18:50:27 +0500

    fix

--- src/base-configs/common/common-jest-options.js
+++ src/base-configs/common/common-jest-options.js
@@ -2,7 +2,7 @@ module.exports = {
   coverageProvider: 'v8',
   coverageDirectory: "coverage",
   rootDir: "src",
-  moduleDirectories: ['node_modules', '.'],
+  moduleDirectories: ['node_modules', '../'],
   transformIgnorePatterns: [
     "node_modules/(?!(@buildozer)/)",
   ],
