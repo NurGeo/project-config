15a06713e06c21602289ad172b245e0819d2cc87
fx-it <ura-roma@mail.ru>
2021-06-22 13:26:02 +0500

    Update .eslintrc.js

--- src/common-files/backend/.eslintrc.js
+++ src/common-files/backend/.eslintrc.js
@@ -1,4 +1,4 @@
-const baseRules = require("./node_modules/@buildozer/project-configs/src/base-configs/frontend/eslintrc");
+const baseRules = require("./node_modules/@buildozer/project-configs/src/base-configs/backend/eslintrc");
 
 module.exports = {
   ...baseRules,
