235feb083feb0a885a14c4cb8e0a4d9f6276d6b7
timonit <67180983+timonit@users.noreply.github.com>
2021-07-16 15:34:24 +0500

    Убрано jasmine "наследование" конфигов для *.ts,*.js файлов

--- src/base-configs/frontend/.eslintrc.js
+++ src/base-configs/frontend/.eslintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   overrides: [
     {
       extends: [
-        'plugin:jasmine/recommended',
         '../common/.eslintrc.js',
       ],
       files: [
@@ -22,7 +21,6 @@ module.exports = {
     },
     {
       extends: [
-        'plugin:jasmine/recommended',
         '../common/.eslintrc.js',
       ],
       files: [
