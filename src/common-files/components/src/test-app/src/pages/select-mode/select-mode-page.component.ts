d887de962e58a146825dfe4f1c1e0c78f2c6ae8a
timonit <67180983+timonit@users.noreply.github.com>
2021-12-06 12:14:12 +0500

    Добавлен тест-апп разделенный на мобильную и веб версию

---
+++ src/common-files/components/src/test-app/src/pages/select-mode/select-mode-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'base-base-page',
+  templateUrl: './select-mode-page.component.html',
+  styleUrls: ['./select-mode-page.component.scss'],
+})
+export class SelectModePageComponent {
+}
