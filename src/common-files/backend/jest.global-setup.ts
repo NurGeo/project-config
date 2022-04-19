570aed3a538c31f4d2115270f9ee66e16630012c
yur.lug <ura-roma@mail.ru>
2022-04-19 14:50:39 +0500

    fix

--- src/common-files/backend/jest.global-setup.ts
+++ src/common-files/backend/jest.global-setup.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata';
 // Import fs utility
 // import { fsUtility } from './src/utility/fs-utility';
 
-export default async function ():Promise<void> {
-  fsUtility.removeSync(`${process.cwd()}/.temp/tests`);
+export default async function ():  Promise<void> {
+  // Uncomment this
+  // fsUtility.removeSync(`${process.cwd()}/.temp/tests`);
 }
