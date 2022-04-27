57eb557916b00f394371266c47eccad5b965031b
yur.lug <ura-roma@mail.ru>
2022-04-27 11:36:33 +0500

    babel jest снова ушёл, для ts-jest применяется флаг isolatedModules, и теперь скорость юнит тестов на уровне babel-jest

--- src/base-configs/backend/jest.unit-config.js
+++ src/base-configs/backend/jest.unit-config.js
@@ -4,7 +4,4 @@ const jestUnitConfig = require('../common/jest.unit-config');
 module.exports = {
   ...backendJestOptions,
   ...jestUnitConfig,
-  transform: {
-    "\\.ts$": 'babel-jest',
-  },
 };
--- src/base-configs/contracts/jest.unit-config.js
+++ src/base-configs/contracts/jest.unit-config.js
@@ -4,7 +4,4 @@ const jestUnitConfig = require('../common/jest.unit-config');
 module.exports = {
   ...contractsJestOptions,
   ...jestUnitConfig,
-  transform: {
-    "\\.ts$": 'babel-jest',
-  },
 };
